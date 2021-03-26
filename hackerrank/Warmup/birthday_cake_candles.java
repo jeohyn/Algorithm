e892fffc9f5f47c4bf646c2eff70dfa98891a7ad
jeohyn <chajhyeon@gmail.com>
2021-03-26 15:04:00 +0900

    Birthday Cake Candles

---
+++ hackerrank/Warmup/birthday_cake_candles.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'birthdayCakeCandles' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY candles as parameter.
+     */
+
+    public static int birthdayCakeCandles(List<Integer> candles) {
+    // Write your code here
+        Collections.sort(candles);
+        Collections.reverse(candles);
+        int index=1;
+        int max=candles.get(0);
+        int res=1;
+        if(candles.size()==1) return res;
+        int sec=candles.get(index);
+        while(max==sec){
+            res++;
+            index++;
+            if(candles.size()>index)sec=candles.get(index);
+            else return res;
+        }
+        return res;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int candlesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] candlesTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> candles = new ArrayList<>();
+
+        for (int i = 0; i < candlesCount; i++) {
+            int candlesItem = Integer.parseInt(candlesTemp[i]);
+            candles.add(candlesItem);
+        }
+
+        int result = Result.birthdayCakeCandles(candles);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
