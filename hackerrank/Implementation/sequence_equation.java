c236b18835474af1d757ed89b3cbeb96984c19ce
jeohyn <chajhyeon@gmail.com>
2021-04-27 15:06:24 +0900

    Sequence Equation

---
+++ hackerrank/Implementation/sequence_equation.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the permutationEquation function below.
+    static int[] permutationEquation(int[] p) {
+        int lens=p.length;
+        int [] res=new int[lens];
+        for(int i=1; i<=lens;i++){
+            int k=searchElement(p, i);
+            int a=searchElement(p, k+1);
+            res[i-1]=a+1;
+        }
+        return res;
+    }
+    
+    static int searchElement(int [] p, int x){
+        for(int i=0;i<p.length;i++){
+            if(p[i]==x) return i;
+        }
+        return -1;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] p = new int[n];
+
+        String[] pItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int pItem = Integer.parseInt(pItems[i]);
+            p[i] = pItem;
+        }
+
+        int[] result = permutationEquation(p);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
